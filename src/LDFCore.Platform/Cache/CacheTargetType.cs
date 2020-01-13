3e4d5745c638d23bfee744e3288f75777b92b6a9
liufayuan <fayuanliu@outlook.com>
2020-01-13 18:42:11 +0800

    初始化项目

---
+++ src/LDFCore.Platform/Cache/CacheTargetType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LDFCore.Platform.Cache
+{
+    public enum CacheTargetType
+    {
+        MemoryCache,
+        Redis,
+    }
+}
